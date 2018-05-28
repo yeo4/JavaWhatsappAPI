b8e2de7040f3dfbe9742d89c8141730a73d26e3a
yeo4 <yeo4.jo@gmail.com>
2018-05-28 17:07:50 +0300

    Create Message.java

---
+++ Message.java
@@ -0,0 +1,26 @@
+package bot;
+
+public class Message {
+	private String id;
+	private String contact;
+	private String body;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+}
